174b722943cd6e0e9dc176663425c675afb8021b
kartik1502 <kartikkulkarni1411@gmail.com>
2023-07-01 15:48:48 +0530

    added the creation of the student functionality in the Student service

---
+++ Student Service/src/main/java/org/training/studentservice/entity/Student.java
@@ -0,0 +1,28 @@
+package org.training.studentservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    private String studentId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String emailId;
+
+    private String contactNo;
+}
