7c69fd8a115e598270e667eaa84e70b045ccc463
kartik1502 <kartikkulkarni1411@gmail.com>
2023-07-09 20:48:35 +0530

    implemented the FeignClientErrorDecoder to identify the exception thrown in called services and handle them in the caller service.

--- Student Service/src/main/java/org/training/studentservice/external/MentorService.java
+++ Student Service/src/main/java/org/training/studentservice/external/MentorService.java
@@ -3,9 +3,10 @@ package org.training.studentservice.external;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.training.studentservice.config.FeignClientConfiguration;
 import org.training.studentservice.dto.MentorDto;
 
-@FeignClient(name = "MENTOR-SERVICE")
+@FeignClient(name = "MENTOR-SERVICE", configuration = FeignClientConfiguration.class)
 public interface MentorService {
 
     @GetMapping("/mentors/{mentorId}")
